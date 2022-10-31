3078008bc69142071044724996034de0462ed184
Abdur Rakib <89575718+Hsjeksjsy68@users.noreply.github.com>
2022-10-31 01:41:37 -1200

    Add files via upload

---
+++ match.js
@@ -0,0 +1,4 @@
+document.getElementById('line-1-section-1').innerHTML = ('FC RELDOS');
+document.getElementById('line-1-section-2').innerHTML = (' REAL MADRID');
+document.getElementById('line-2-section-1').innerHTML = ('FC RELDOS');
+document.getElementById('line-2-section-2').innerHTML = (' FC BARCELONA');
